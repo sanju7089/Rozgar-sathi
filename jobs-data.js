baae20fbf5c1053276c4fc72d1359cea6ef68029
sanju7089 <sanjuboby7089@gmail.com>
2026-08-21 11:39:22 +0530

    Create jobs-data.js

---
+++ jobs-data.js
@@ -0,0 +1,21 @@
+const jobs = [
+  {
+    id: "nrlm-2026",
+    title: "NRLM Recruitment 2026",
+    category: "government",
+    qualification: "Post के अनुसार",
+    posts: "202",
+    lastDate: "2026-09-17",
+    page: "nrlm-2026.html"
+  },
+
+  {
+    id: "ssc-chsl",
+    title: "SSC CHSL",
+    category: "government",
+    qualification: "12th Pass",
+    posts: "Notification के अनुसार",
+    lastDate: "2026-09-17",
+    page: "ssc-chsl.html"
+  }
+];
